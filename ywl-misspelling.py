29725cf0f26a667db7a1ba26a574b52f7196ed96
Weilin Ye <weilin@weilindndiannao.mynetworksettings.com>
2024-04-27 21:35:35 -0400

    added frequency check in misspelling checking functions

--- ywl-misspelling.py
+++ ywl-misspelling.py
@@ -1,6 +1,7 @@
 from spellchecker import SpellChecker
 import pandas as pd
 import re
+from collections import Counter
 from edit_distance import edit_distance
 from clustering import hierarchical_clustering
 
@@ -8,7 +9,7 @@ spell = SpellChecker()
 
 
 def load_test_dataset():
-    df = pd.read_csv("./datasets/Volunteers.csv")
+    df = pd.read_csv("./datasets/Volunteers-misspelled.csv")
     pre_processing(df)
     return df
 
@@ -79,11 +80,11 @@ def check_misspelling(line: str, location: pd.DataFrame):
     candidates = spell.unknown(words=words)
     if candidates:
         result = []
-        print("Initially: " + str(candidates))
+        # print("Initially: " + str(candidates))
         for c in candidates:
-            if c.lower() not in location["location"].values:
+            if c.lower() not in location["location"].to_numpy():
                 result.append(c)
-        print("After filtering out locations: " + str(result))
+        # print("After filtering out locations: " + str(result))
         if result:
             return True, result
         else:
@@ -101,22 +102,62 @@ def check_column_misspelling(df: pd.DataFrame, col_name: str, location: pd.DataF
         line = row[col_name]
         if not pd.isna(line):
             flag, candidates = check_misspelling(line=line, location=location)
+            if flag is True:
+                threshold = int(df[col_name].count() / 200)
+                for candidate in candidates:
+                    frequency = check_frequency(candidate, df, col_name)
+                    if frequency < threshold:
+                        flag = True
+                        break
+                    else:
+                        flag = False
+            if flag is True:
+                print(col_name + " misspelling: " + " ".join(candidates))
             row[col_name + ": Misspelling"] = flag
+
+
+def check_frequency(word: str, df: pd.DataFrame, col_name: str):
+    col = pd.DataFrame(df[col_name])
+    col = (
+        col[col_name]
+        .astype("str")
+        .str.lower()
+        .str.replace("[^a-zA-Z]", " ", regex=True)
+        .replace(r"\s+", " ", regex=True)
+        .str.strip()
+    )
+    col.dropna(inplace=True)
+    col = col.str.split(" ")
+    col = col.explode()
+    frequency = (col == word.lower()).sum()
+    return frequency
+
 
 def clustering_misspelling_check(df: pd.DataFrame, col_name: str):
     """
-    check misspelling using the heuristic method: 
+    check misspelling using the heuristic method:
     clustering the words, the word with low frequency within a cluster might be misseplling
     """
     words = df[col_name].dropna().to_list()
-    cluster_dict = hierarchical_clustering(words = words, dist=edit_distance)
-    for label, cluster in cluster_dict.items():
-        print(f"Cluster {label}: {', '.join(cluster)}")
+    words = list(map(lambda x: re.sub("[^a-zA-Z ]", " ", x), words))
+    words = list(map(lambda x: re.sub(r"\s+", " ", x).strip(), words))
+    words = list(map(lambda x: x.lower(), words))
+    words = list(map(lambda x: x.split(" "), words))
+    flatten_words = []
+    for word_list in words:
+        for word in word_list:
+            flatten_words.append(word)
+    cluster_dict = hierarchical_clustering(words=flatten_words, dist=edit_distance)
+    # cluster_dict = {cluster_label: [words]}
+    for _, cluster in cluster_dict.items():
+        counter = Counter(cluster)
+        print(counter)
+
 
 if __name__ == "__main__":
     df = load_test_dataset()
     locations = load_location()
     locations = flatten_location(locations)
-    # check_column_misspelling(df, "NTA", locations)
-    clustering_misspelling_check(df, "NTA")
-
+    check_column_misspelling(df, "NTA", locations)
+    # clustering_misspelling_check(df, "NTA")
+    # print(check_frequency("cuty", df, "NTA"))
