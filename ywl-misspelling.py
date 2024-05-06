66afaa32a7ff6eb8bd1f98bd018cdc61018ffd86
Weilin Ye <weilin@Weilindebijibendiannao.local>
2024-05-05 23:28:22 -0400

    evaluate result

--- ywl-misspelling.py
+++ ywl-misspelling.py
@@ -9,7 +9,8 @@ spell = SpellChecker()
 
 
 def load_test_dataset():
-    df = pd.read_csv("./datasets/Volunteers-misspelled.csv")
+    # df = pd.read_csv("./datasets/Volunteers-misspelled.csv")
+    df = pd.read_csv("./datasets/VolunteersMisspelling.csv")
     pre_processing(df)
     return df
 
@@ -98,7 +99,7 @@ def check_column_misspelling(df: pd.DataFrame, col_name: str, location: pd.DataF
     check each row of one column and add tag in a new column
     """
     df[col_name + ": Misspelling"] = False
-    for _, row in df.iterrows():
+    for index, row in df.iterrows():
         line = row[col_name]
         if not pd.isna(line):
             flag, candidates = check_misspelling(line=line, location=location)
@@ -113,7 +114,8 @@ def check_column_misspelling(df: pd.DataFrame, col_name: str, location: pd.DataF
                         flag = False
             if flag is True:
                 print(col_name + " misspelling: " + " ".join(candidates))
-            row[col_name + ": Misspelling"] = flag
+            # row[col_name + ": Misspelling"] = flag
+            df.at[index, col_name + ": Misspelling"] = flag
 
 
 def check_frequency(word: str, df: pd.DataFrame, col_name: str):
@@ -154,10 +156,77 @@ def clustering_misspelling_check(df: pd.DataFrame, col_name: str):
         print(counter)
 
 
+def show_tags(df: pd.DataFrame, col_name: str, tag_name: str):
+    """
+    Show the tag of one column and show the original column if the tag is true
+    """
+    for _, row in df.iterrows():
+        tag = row[col_name + ": " + tag_name]
+        if not pd.isna(tag):
+            # print(tag)
+            if tag is True:
+                print(row[col_name])
+
+
+def check_accuracy(df: pd.DataFrame):
+    col_name = "NTA"
+    tag_name = "Misspelling"
+    num_misspelling = 6
+    total = df[col_name].count()
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
+    misspelling = [
+        "Batteru Park City-Lower Manhattan",
+        "Van Cortlandt Villafe",
+        "SoHo-TriBeCa-Civic Center-Littl Italy",
+        "Crown Hights North",
+        "Huntors Point-Sunnyside-West Maspeth",
+        "Mariner's Habor-Arlington-Port Ivory-Graniteville",
+        "Eeast Flushing",
+    ]
+    for index, row in df.iterrows():
+        tag = row[col_name + ": " + tag_name]
+        nta = row[col_name]
+        if tag is True:
+            # print(nta)
+            if nta in misspelling:
+                true_positive += 1
+            else:
+                false_positive += 1
+        else:
+            if pd.isna(nta):
+                true_negative += 1
+            elif nta in misspelling:
+                print(nta)
+                false_negative += 1
+            else:
+                true_negative += 1
+    true_positive_rate = true_positive / (true_positive + false_negative)
+    true_negative_rate = true_negative / (true_negative + false_positive)
+    false_positive_rate = false_positive / (false_positive + true_negative)
+    false_negative_rate = false_negative / (false_negative + true_positive)
+    accuracy = (true_positive + true_negative) / (
+        true_positive + true_negative + false_positive + false_negative
+    )
+
+    print("True Positive Count:", true_positive)
+    print("True Negative Count:", true_negative)
+    print("False Positive Count:", false_positive)
+    print("False Negative Count:", false_negative)
+
+    print("True Positive Rate (Sensitivity or Recall):", true_positive_rate)
+    print("True Negative Rate (Specificity):", true_negative_rate)
+    print("False Positive Rate (Fall-out):", false_positive_rate)
+    print("False Negative Rate (Miss Rate):", false_negative_rate)
+    print("Accuracy:", accuracy)
+
+
 if __name__ == "__main__":
     df = load_test_dataset()
     locations = load_location()
     locations = flatten_location(locations)
     check_column_misspelling(df, "NTA", locations)
-    # clustering_misspelling_check(df, "NTA")
-    # print(check_frequency("cuty", df, "NTA"))
+    # show_tags(df, "NTA", "Misspelling")
+    check_accuracy(df)
