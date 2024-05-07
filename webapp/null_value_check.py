88e60d074bb92d6bf8ffb950699374550c025d82
Donglin Zhou <zhdl1999@gmail.com>
2024-05-07 15:25:59 -0400

    update api

--- webapp/null_value_check.py
+++ webapp/null_value_check.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-def null_value_check(df, df_out, columns):
+def null_value_check(df, columns):
     mask = df.isna().any(axis=1)
 
-    filtered_out = df[mask]
+    # filtered_out = df[mask]
     filtered = df[~mask]
-    filtered_out = pd.concat([filtered_out, df_out], ignore_index=True)
-    return filtered, filtered_out
+    # filtered_out = pd.concat([filtered_out, df_out], ignore_index=True)
+    return filtered
